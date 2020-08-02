d412f6842158a757b64f29964672bc87ef40189e
Thai <elemti.lmt@gmail.com>
2020-08-02 13:34:30 +0700

    add extra boilerplate configs

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "extends": "react-app",
+  "globals": {
+  },
+  "rules": {
+    "no-console": "off",
+    "no-constant-condition": "off",
+    "no-unreachable": "off",
+    "no-empty": "off",
+    "no-useless-escape": "off",
+    "no-throw-literal": "off",
+    "no-unused-vars": "warn",
+  }
+};
+
