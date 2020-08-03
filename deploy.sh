bef8a6163cefefa7922c993d6e33648c5d17085b
Thai <elemti.lmt@gmail.com>
2020-08-03 08:25:29 +0700

    test

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,7 @@ ssh -i ssh.key -o "StrictHostKeyChecking=no" root@oddle-challenge.elemti.com "\
   pm2 -s delete oddle-challenge; \
   rm -rf oddle-challenge; \
   mkdir -p oddle-challenge; \
-  tar xvf $BUNDLE -C oddle-challenge"
+  tar xvf $BUNDLE -C oddle-challenge && \
+  npm i -g serve && \
+  pm2 -s start --name oddle-challenge --time npx -- serve -s build -p 80 && \
+  pm2 save"
